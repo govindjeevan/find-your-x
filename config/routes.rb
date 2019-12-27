852d3e1b09a282c301dba2d19171a78198f836a2
Govind Jeevan <govindjeevan7@gmail.com>
2019-12-28 04:42:20 +0530

    new route

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
       get 'experience'
     end
   end
+
+  get "play", :controller=>"markers", :action=>"experience"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
