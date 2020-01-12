ecb5dd03142e0b45468c7d846f1a3fdae82488e8
Pavan Vachhani <vachhanihpavan@gmail.com>
2020-01-12 15:14:51 +0530

    Added Google Auth and name column to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_12_090120) do
+ActiveRecord::Schema.define(version: 2020_01_12_093943) do
 
   create_table "markers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.decimal "lat", precision: 15, scale: 13
@@ -49,8 +49,12 @@ ActiveRecord::Schema.define(version: 2020_01_12_090120) do
     t.boolean "speaker_11", default: false
     t.boolean "speaker_12", default: false
     t.boolean "admin", default: false
+    t.string "provider"
+    t.string "uid"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
 end
