0a20877277303d00b07fc26eddef90ccb2cdece2
Pavan Vachhani <vachhanihpavan@gmail.com>
2020-01-12 15:25:12 +0530

    Changed precision and scale

--- db/migrate/20200112090120_change_marker_precision.rb
+++ db/migrate/20200112090120_change_marker_precision.rb
@@ -1,6 +1,6 @@
 class ChangeMarkerPrecision < ActiveRecord::Migration[5.2]
   def change
-    change_column :markers, :lat, :decimal, :precision => 15, :scale => 13
-    change_column :markers, :lng, :decimal, :precision => 15, :scale => 13
+    change_column :markers, :lat, :decimal, :precision => 6, :scale => 4
+    change_column :markers, :lng, :decimal, :precision => 6, :scale => 4
   end
 end
