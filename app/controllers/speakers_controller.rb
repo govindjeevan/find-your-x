b1e534c5c61f6464d5edb8393a3d6ca0e0d87a30
Bharath Adikar <lkcbharath@gmail.com>
2020-01-13 11:30:02 +0530

    Added instagram, fixed name saving

--- app/controllers/speakers_controller.rb
+++ app/controllers/speakers_controller.rb
@@ -74,7 +74,7 @@ class SpeakersController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def speaker_params
-    params.require(:speaker).permit(:name, :title, :desc)
+    params.require(:speaker).permit(:name, :title, :desc, :insta)
   end
 
   def check_show_access
